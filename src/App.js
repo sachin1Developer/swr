efde6a2e16576ff6bad3d1a5dd8d17788f5ce0ec
sachin1Developer <sachin.1@telemune.com>
2024-07-05 12:24:45 +0530

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     url: 'https://jsonplaceholder.typicode.com/todos/',
     options: {
       revalidateOnMount: false,
-       // Prevent automatic fetch on mount
+      // Prevent automatic fetch on mount
     }
   });
 
@@ -18,15 +18,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <button onClick={onSubmit}>Fetch Todos</button>
-      {loadingData && <p>Loading...</p>}
-      {errorData && <p>Error: {errorData.message}</p>}
-      {isValidating && <p>Validating...</p>}
+      <div className='flex justify-between' >
+        <h1 className='text-3xl'>Todo List</h1>
+
+        {loadingData && <p>Loading...</p>}
+        {errorData && <p>Error: {errorData.message}</p>}
+        {isValidating && <p>Validating...</p>}
+        <button onClick={onSubmit} className='bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg' >Fetch Todos</button>
+      </div>
+      <hr className='m-2' />
       {allData && (
         <ul>
           {allData.map(todo => (
-            <li key={todo.id}>{todo.title}</li>
+            <li className='' key={todo.id}>{todo.title}</li>
           ))}
         </ul>
       )}
